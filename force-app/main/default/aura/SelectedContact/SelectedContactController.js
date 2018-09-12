8660885ccc83c79c1f5e6f11feda49a10efd9c9a
Pat McClellan <pat@proton7group.com>
2018-09-12 10:33:26 -0700

    test

--- force-app/main/default/aura/SelectedContact/SelectedContactController.js
+++ force-app/main/default/aura/SelectedContact/SelectedContactController.js
@@ -1,4 +1,25 @@
 ({
+    init : function(component, event, helper) {
+        var navService = component.find("navService");
+        // Sets the route to /lightning/o/Account/view
+        var pageReference = {
+            type: 'standard__objectPage',
+            attributes: {
+                objectApiName: 'Contact',
+                actionName: 'view'
+            }
+        };
+        component.set("v.pageReference", pageReference);
+        // Set the URL on the link or use the default if there's an error
+        var defaultUrl = "#";
+        navService.generateUrl(pageReference)
+            .then($A.getCallback(function(url) {
+                component.set("v.url", url ? url : defaultUrl);
+            }), $A.getCallback(function(error) {
+                component.set("v.url", defaultUrl);
+            }));
+    },
+    
     onSelectContact : function(component, event, helper) {
         var contact = event.getParam("Contact");
         component.set("v.Contact", contact);
@@ -9,7 +30,7 @@
     handleClick: function(component, event, helper) {
         var navService = component.find("navService");
         // Uses the pageReference definition in the init handler
-        var pageReference = component.get("v.Contact.Id");
+        var pageReference = component.get("v.url");
         event.preventDefault();
         navService.navigate(pageReference);
     },
