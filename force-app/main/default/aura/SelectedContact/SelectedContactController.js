914124a594480e5933c773a9a67d17fc42242945
Pat McClellan <pat@proton7group.com>
2018-09-12 10:38:39 -0700

    helper

--- force-app/main/default/aura/SelectedContact/SelectedContactController.js
+++ force-app/main/default/aura/SelectedContact/SelectedContactController.js
@@ -1,32 +1,11 @@
 ({
-    initNav : function(component, event, helper) {
-        var navService = component.find("navService");
-        // Sets the route to /lightning/o/Account/view
-        var pageReference = {
-            type: 'standard__objectPage',
-            attributes: {
-                objectApiName: 'Contact',
-                actionName: 'view'
-            }
-        };
-        component.set("v.pageReference", pageReference);
-        // Set the URL on the link or use the default if there's an error
-        var defaultUrl = "#";
-        navService.generateUrl(pageReference)
-            .then($A.getCallback(function(url) {
-                component.set("v.url", url ? url : defaultUrl);
-            }), $A.getCallback(function(error) {
-                component.set("v.url", defaultUrl);
-            }));
-        console.log("Url: " + component.get("v.url"));
-    },
     
     onSelectContact : function(component, event, helper) {
         var contact = event.getParam("Contact");
         component.set("v.Contact", contact);
         component.set("v.cardTitle", contact.Name);
         component.set("v.hasContact", true);
-        component.initNav(component, event, helper);
+        helper.initNav(component);
     },
     
     handleClick: function(component, event, helper) {
--- force-app/main/default/aura/SelectedContact/SelectedContactHelper.js
+++ force-app/main/default/aura/SelectedContact/SelectedContactHelper.js
@@ -1,5 +1,24 @@
 ({
-    helperMethod : function() {
-
-    }
+    initNav : function(component) {
+        var navService = component.find("navService");
+        // Sets the route to /lightning/o/Account/view
+        var pageReference = {
+            type: 'standard__objectPage',
+            attributes: {
+                objectApiName: 'Contact',
+                actionName: 'view'
+            }
+        };
+        component.set("v.pageReference", pageReference);
+        // Set the URL on the link or use the default if there's an error
+        var defaultUrl = "#";
+        navService.generateUrl(pageReference)
+            .then($A.getCallback(function(url) {
+                component.set("v.url", url ? url : defaultUrl);
+            }), $A.getCallback(function(error) {
+                component.set("v.url", defaultUrl);
+            }));
+        console.log("Url: " + component.get("v.url"));
+    },
+    
 })
