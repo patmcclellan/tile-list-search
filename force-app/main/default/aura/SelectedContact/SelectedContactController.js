d05dfb96bb2163b7d3536c04580f276b810c27c4
Pat McClellan <pat@proton7group.com>
2018-09-12 08:47:04 -0700

    selectedContactcard

--- force-app/main/default/aura/SelectedContact/SelectedContactController.js
+++ force-app/main/default/aura/SelectedContact/SelectedContactController.js
@@ -1,5 +1,9 @@
 ({
-    myAction : function(component, event, helper) {
+    onSelectContact : function(component, event, helper) {
+        var contact = event.getParam("Contact");
 
+        // set the handler attributes based on event data
+        component.set("v.Contact", contact);
+        component.set("v.hasContact", true);
     }
 })
