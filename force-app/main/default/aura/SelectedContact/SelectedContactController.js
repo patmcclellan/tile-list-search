2c61604f395b12607bee10e6b8548a24e791bfbb
Pat McClellan <pat@proton7group.com>
2018-09-12 10:29:05 -0700

    link

--- force-app/main/default/aura/SelectedContact/SelectedContactController.js
+++ force-app/main/default/aura/SelectedContact/SelectedContactController.js
@@ -4,5 +4,16 @@
         component.set("v.Contact", contact);
         component.set("v.cardTitle", contact.Name);
         component.set("v.hasContact", true);
-    }
+    },
+    
+    handleClick: function(component, event, helper) {
+        var navService = component.find("navService");
+        // Uses the pageReference definition in the init handler
+        var pageReference = component.get("v.Contact.Id");
+        event.preventDefault();
+        navService.navigate(pageReference);
+    },
+
+    //more code here
+ 
 })
